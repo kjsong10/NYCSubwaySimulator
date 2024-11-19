9ff37807f102b0644d908df111cd4ea3f552b9e1
Dimosthenis Tsingounis <dtsingo000@citymail.cuny.edu>
2024-11-18 21:16:43 -0500

    NYCSubwaySimulator

--- area_3d.gd
+++
@@ -1,8 +0,0 @@
-extends Area3D
-
-# Set the default respawn position for the player
-var default_position = Vector3(0, 0, 0)  # Replace with your desired coordinates
-
-func _on_area_entered(area):
-	if area.is_in_group("player"):  # Make sure your player is in the "player" group
-		area.global_transform.origin = default_position  # Teleport the player
