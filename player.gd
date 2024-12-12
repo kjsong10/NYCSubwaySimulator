fa3e38adb3de1e2108c4173790aacecb96867a86
Dimosthenis Tsingounis <dtsingo000@citymail.cuny.edu>
2024-12-12 13:01:40 -0500

    Main Menu, Paused Menu, Volume, Controls, Resolution
    Main Menu, Paused Menu, Volume, Controls, Resolution

--- player.gd
+++ player.gd
@@ -7,7 +7,9 @@ const JUMP_VELOCITY = 6
 @onready var neck := $Neck
 @onready var camera := $Neck/Camera
 @onready var win_message_label = $"/root/Main/CanvasLayer/Label"
+
 @onready var train_arrived = false
+
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
@@ -26,7 +28,7 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 	if Input.is_action_pressed("turn_left"):
 		neck.rotate_y(0.04)
@@ -68,6 +70,7 @@ func _on_win_body_exited(body: Node3D) -> void:
 	win_message_label.visible = false
 
 
+
 func _on_go_to_137_body_entered(body: Node3D) -> void:
 	print("going to 137")
 	if(get_tree() != null):
@@ -91,3 +94,13 @@ func _on_summon_train_body_entered(body: Node3D) -> void:
 		var animPlayer: AnimationPlayer = $"/root/137/Train/AnimationPlayer"
 		animPlayer.play("Enter Station")
 		train_arrived = true
+
+func open_map():
+	# Convert the Godot-specific path to a system path
+	var path = ProjectSettings.globalize_path("res://API/index.html")
+	OS.shell_open(path)
+	
+func _input(event):
+	if event.is_action_pressed("map_open"):
+		open_map()
+		
